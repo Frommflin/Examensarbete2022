a8fdb5fb897eee7841fe162cfbb269c5a9967263
a19klafr <a19klafr@student.his.se>
2022-03-15 15:55:22 +0100

    Added 'new user'-messaging
    Added messaging between server and clients when new user has connected and submitted a username

--- WebSocket/server/index.js
+++ WebSocket/server/index.js
@@ -1,10 +1,37 @@
-const WebSocket = require('ws');
+const WebSocket = require(`ws`);
 const server = new WebSocket.Server({ port: 8082 });
 
-server.on('connection', client => {
-    console.log('New client connected!');
+server.on(`connection`, ws => {
+    console.log(`New client connected!`);
     
-    client.on('close', () => {
-        console.log('Client has disconnected');
+    ws.on(`close`, () => {
+        console.log(`Client has disconnected`);
+    });
+
+    ws.on(`message`, data => {
+        var message;
+        var response;
+        try {
+            message = JSON.parse(data);
+        } catch (error) {
+            console.log(error);
+        }
+        //Different handlers depending on message type
+        switch(message.type) { 
+            case `NEW_USER`: 
+                console.log(`Client has submitted a namne: ${message.name}`);
+                response = {
+                    type: "new_user",
+                    name: message.name
+                };
+                server.clients.forEach((client) => {
+                    if ((client != ws) && (client.readyState === WebSocket.OPEN)) {
+                        client.send(JSON.stringify(response));
+                    }
+                });
+                break;  
+            default: 
+                break; 
+        }; 
     })
 });
